0e395ec5f7e4937981baf4dfef7776257f054d20
Francis T. O'Donovan <proinsias@users.noreply.github.com>
2021-04-24 18:02:56 -0400

    Address pre-commit issues

--- tests/plaidml_keras_vgg19_test.py
+++ tests/plaidml_keras_vgg19_test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
 From https://github.com/plaidml/plaidml/blob/master/README.md.
@@ -8,7 +9,7 @@ import numpy as np
 import os
 import time
 
-os.environ["KERAS_BACKEND"] = "plaidml.keras.backend"  # noqa: E402 module level import not at top
+os.environ['KERAS_BACKEND'] = 'plaidml.keras.backend'  # noqa: E402 module level import not at top
 
 import keras.applications as kapp
 from keras.datasets import cifar10
@@ -20,18 +21,20 @@ def test_plaidml_keras_vgg19():
     x_train = x_train[:batch_size]
     x_train = np.repeat(np.repeat(x_train, 7, axis=1), 7, axis=2)
     model = kapp.VGG19()
-    model.compile(optimizer='sgd', loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(
+        optimizer='sgd', loss='categorical_crossentropy',
+        metrics=['accuracy'],
+    )
 
-    print("Running initial batch (compiling tile program)")
+    print('Running initial batch (compiling tile program)')
     model.predict(x=x_train, batch_size=batch_size)
 
     # Now start the clock and run 10 batches
-    print("Timing inference...")
+    print('Timing inference...')
     start = time.time()
     for i in range(10):
         model.predict(x=x_train, batch_size=batch_size)
-    print("Ran in {} seconds".format(time.time() - start))
+    print('Ran in {} seconds'.format(time.time() - start))
 
 
 if __name__ == '__main__':
